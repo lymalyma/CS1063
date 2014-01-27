0f071c38de655c18dd853e02e0b4dee94f3b3be8
Lymari Montijo <lymari.montijo@gmail.com>
2014-01-26 23:59:16 -0600

    Added answers for lab0

---
+++ lab0/Initials.java
@@ -0,0 +1,17 @@
+public class Initials {
+  public static void main(String[] args) {
+    System.out.println("Lab 0 done by Lymari Montijo");
+    System.out.println();
+    System.out.println("L          QQQQQQ      M       M"); 
+    System.out.println("L         Q      Q     M M   M M");
+    System.out.println("L         Q      Q     M  M M  M");
+    System.out.println("L         Q      Q     M   M   M");
+    System.out.println("L         Q   Q  Q     M       M");
+    System.out.println("L         Q    Q Q     M       M");
+    System.out.println("LLLLLLL    QQQQQQ      M       M");
+    System.out.println("                 Q");
+    
+    /*This short and simple Java program prints out my name's initials 
+     in block letters using the method System.out.println()*/
+  }
+}
